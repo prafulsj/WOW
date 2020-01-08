6780c98dd355af873a16e08f8a2cc21da178e255
Praful <rslnew@Praful-Mac-mini.local>
2020-01-08 11:12:15 +0530

    init

---
+++ Podfile
@@ -0,0 +1,7 @@
+use_frameworks!
+
+target 'WOW' do
+  pod 'Alamofire', '~> 5.0.0-rc.3'
+  pod 'SwiftyJSON', '~> 4.0'
+  pod 'LayoutKit'
+end
---
+++ WOW/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  WOW
+//
+//  Created by Praful on 07/01/20.
+//  Copyright © 2020 Praful. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
